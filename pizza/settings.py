cfeda70877b3e63658157bcea0a3bf7d18583d5d
vitoria-ana <vitorialcrispim@hotmail.com>
2026-06-15 21:05:59 -0300

    Django key gets revoked

--- pizza/settings.py
+++ pizza/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bqf83_mdn-$o8_xib8)tduwoekz@k^z-^%j=$k++p5$py(vvu('
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
